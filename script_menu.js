93906caa45e1a2e850452583438435b933fb666f
san-eva01 <sesan950@gmail.com>
2025-05-22 00:17:50 -0600

    menu

--- script_menu.js
+++ script_menu.js
@@ -12,4 +12,45 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     });
+
+    // Navegación para las tarjetas del menú
+    const cards = document.querySelectorAll(".card");
+    cards.forEach(card => {
+        card.addEventListener("click", function() {
+            const categoria = this.querySelector("h4").textContent;
+            let pagina = "";
+            
+            switch(categoria) {
+                case "ENTRADAS":
+                    pagina = "entradas.html";
+                    break;
+                case "ENSALADAS":
+                    pagina = "ensaladas.html";
+                    break;
+                case "CONTEMPORÁNEO":
+                    pagina = "contemporaneo.html";
+                    break;
+                case "PREHISPÁNICO":
+                    pagina = "prehispanico.html";
+                    break;
+                case "EXÓTICO":
+                    pagina = "exotico.html";
+                    break;
+                case "COCTELERÍA":
+                    pagina = "cocteleria.html";
+                    break;
+                case "BEBIDAS":
+                    pagina = "bebidas.html";
+                    break;
+                case "POSTRES":
+                    pagina = "postres.html";
+                    break;
+                default:
+                    console.log("Categoría no encontrada");
+                    return;
+            }
+            
+            window.location.href = pagina;
+        });
+    });
 });
